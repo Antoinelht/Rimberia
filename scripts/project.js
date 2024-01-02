a3f70fbae7fc23176a16dc38f20c4c9084ddc97c
unknown <laheurte.antoine@hotmail.fr>
2024-01-02 09:41:13 +0100

    update

--- scripts/project.js
+++ scripts/project.js
@@ -1,68 +1,68 @@
+// === constants ===
 const MAX_QTY = 9;
 const MIN_QTY = 0;
 const productIdKey = "product";
 const orderIdKey = "order";
 const inputIdKey = "qte";
 
-
 // === global variables === 
 // the total cost of selected products 
-
-
 let total = 0;
 
+// function called when page is loaded, it performs initializations 
 let init = function () {
 	createShop();
 } 
 window.addEventListener("load", init);
 
+// usefull functions
+/*
+* create and add all the div.produit elements to the div#boutique element
+* according to the product objects that exist in 'catalog' variable
+*/
 let createShop = function () {
 	let shop = document.getElementById("boutique");
-	for (i = 0; i < catalog.length; i++) {
+	for(i = 0; i < catalog.length; i++) {
 		shop.appendChild(createProduct(catalog[i], i));
 	}
 }
-
 /*
 * create the div.produit elment corresponding to the given product
 * The created element receives the id "index-product" where index is replaced by param's value
 * @param product (product object) = the product for which the element is created
 * @param index (int) = the index of the product in catalog, used to set the id of the created element
 */
-
 let createProduct = function (product, index) {
+	// build the div element for product
 	let block = document.createElement("div");
 	block.className = "produit";
+	// set the id for this product
 	block.id = index + "-" + productIdKey;
+	// build the h4 part of 'block'
 	block.appendChild(createBlock("h4", product.name));
-
 	// /!\ should add the figure of the product... does not work yet... /!\ 
 	block.appendChild(createFigureBlock(product));
 	// build and add the div.description part of 'block' 
-
 	block.appendChild(createBlock("div", product.description, "description"));
+	// build and add the div.price part of 'block'
 	block.appendChild(createBlock("div", product.price, "prix"));
+	// build and add control div block to product element
 	block.appendChild(createOrderControlBlock(index));
 	return block;
 }
-
 /* return a new element of tag 'tag' with content 'content' and class 'cssClass'
  * @param tag (string) = the type of the created element (example : "p")
  * @param content (string) = the html wontent of the created element (example : "bla bla")
  * @param cssClass (string) (optional) = the value of the 'class' attribute for the created element
  */
-
-
-
 let createBlock = function (tag, content, cssClass) {
 	let element = document.createElement(tag);
 	if (cssClass != undefined) {
-		element.className = cssClass;
+		element.className =  cssClass;
 	}
 	element.innerHTML = content;
 	return element;
 }
-
 /*
 * builds the control element (div.controle) for a product
 * @param index = the index of the considered product
@@ -74,27 +74,17 @@ let createOrderControlBlock = (index)=> {
 	let control = document.createElement("div");
 	control.className = "controle";
 // create input quantity element
-
-
-let createOrderControlBlock = (index) => {
-	let control = document.createElement("div");
-	control.className = "controle";
-
-
 	let input = document.createElement("input");
 	input.id = index + '-' + inputIdKey;
 	input.type = "number";
 	input.value = "0";
-
 //add event handling
 	input.addEventListener("input", function () {
 			let quantity = parseInt(input.value);
-
 		if (quantity < MIN_QTY || quantity < 0 || quantity > MAX_QTY) {
 			input.value = "0";
 		}
 		updateButtonState(button, quantity);
-
 });
 // create order button
 	let button = document.createElement("button");
@@ -170,17 +160,14 @@ let addToCart = (index, quantity)=> {
 	updateTotal();
 };
 // check the total cart 
-
 let updateTotal = () => {
 	let cartItems = document.querySelectorAll("#panier .achat");
 	let total = 0;
 	cartItems.forEach(function (cartItem) {
-
 			let quantity = parseInt(cartItem.querySelector(".quantite").textContent);
 			let index = cartItem.id.split("-")[1];
 			let product = catalog[index];
 			total += quantity * product.price;
-
 	});
 	total = total.toFixed(2);
 	document.getElementById("montant").textContent = total;
@@ -189,24 +176,18 @@ let updateTotal = () => {
 let createFigureBlock = function (product) {
 	let figure = document.createElement("figure");
 	let image = document.createElement("img");
-
 	image.src = product.image;  
 	image.alt = product.name; 
-
 	figure.appendChild(image);
 
 	return figure;
 };
 document.addEventListener("DOMContentLoaded", function () {
-
 	// // Retrieve the required DOM elements
-
-
 	const filterInput = document.getElementById("filter");
 	const boutiqueDiv = document.getElementById("boutique");
 	const totalSpan = document.getElementById("montant");
 	const achatsDiv = document.querySelector(".achats");
-
 
 	// Product display update function
 	function updateDisplay(products) {
